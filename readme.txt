49e709b195c859aa4dfc15935408dd980e1e01b3
NicoR <NicoR@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-06 13:05:30 +0000

    V 1.0.1
    git-svn-id: http://plugins.svn.wordpress.org/wp-original-media-path/tags/1.0.1@648681 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== WP Original Media Path ===
+Contributors: NicoR, rvola
+Tags: media, upload, path, subdomain, 3.5
+Requires at least: 3.5
+Tested up to: 3.5
+Stable tag: 1.0.1
+License: GPLv2 or later
+
+Restore the fields to change the url and file upload
+
+== Description ==
+
+The new version of Wordpress 3.5 to change the media page, removing two fields for defining the location and the sub-field of media.
+There is a constant wp-config.php, but it does not include sub-domains.
+This plug-in is designed for installations WP virgins.
+Once activated, the plugin will add two values ​​in the database you can change it.
+Once complete you can disable the plug-in and remove it. It only works activation.
+
+== Installation ==
+
+Enable plug-ins installed on a new WP.
+It will redirect you to the options page.
+Change the desired values.
+Well, it's done!
+
+You can disable the plugin and delete it, it will do nothing more in time.
+
+== Changelog ==
+
+= 1.0.1. =
+* Contributors list
+* Text fields
+
+= 1.0.0. =
+* Launch Plugin
---
+++ wp_original_media_path.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: WP Original Media Path
+Description: Restore the fields to change the url and file upload
+Version: 1.0.1
+Date : 06/01/2013
+Author: RVOLA
+Author URI: http://rvola.com
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+function rwp_omp_activation() {
+	if (get_option('upload_path') == '' || get_option('upload_url_path') == '') {
+		update_option('upload_url_path','DOMAIN OR SUBDOMAIN FOR MEDIA',true);
+		update_option('upload_path','PATH FOR UPLOAD FOLDER',true);
+		wp_redirect(admin_url('options-media.php'));exit();
+	}
+}
+register_activation_hook( __FILE__, 'rwp_omp_activation');
