fcb24e09e3a5316c330170c62a63678be025aabf
rvola <rvola@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-01 09:38:33 +0000

    * Erase options fields media after disabling the plugin * Fixed settings button in the plugins page
    git-svn-id: http://plugins.svn.wordpress.org/wp-original-media-path/trunk@706364 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_original_media_path.php
+++ wp_original_media_path.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: WP Original Media Path
 Description: Restore the fields to change the url and file upload
-Version: 1.1.0
+Version: 1.2.0
 Date : 06/01/2013
-Revision : 28/03/2013
+Revision : 01/05/2013
 Author: RVOLA
 Author URI: http://rvola.com
 License: GPLv2 or later
@@ -26,8 +26,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-
-
 function wpomp_activation() {
 	if (get_option('upload_path') == '' || get_option('upload_url_path') == '') {
 		update_option('upload_path', 'wp-content/uploads', true);
@@ -36,9 +34,17 @@ function wpomp_activation() {
 }
 register_activation_hook( __FILE__, 'wpomp_activation');
 
-function wpomp_settings_link($links) { 
-	$settings_link = '<a href="'.admin_url('options-media.php').'">'.__('Settings').'</a>'; 
-	array_unshift($links, $settings_link); 
+function wpomp_desactivation() {
+	update_option('upload_path', '', true);
+	update_option('upload_url_path', '', true);
+}
+register_deactivation_hook( __FILE__, 'wpomp_desactivation');
+
+function wpomp_settings_link($links, $file) { 
+	if ( $file == plugin_basename(dirname(__FILE__)).'/wp_original_media_path.php' ) {
+	   $settings_link = '<a href="'.admin_url('options-media.php').'">'.__('Settings').'</a>'; 
+	   array_unshift($links, $settings_link); 
+	}
 	return $links; 
 }
-add_filter("plugin_action_links", 'wpomp_settings_link' );
+add_filter("plugin_action_links", 'wpomp_settings_link', 10, 2 );
