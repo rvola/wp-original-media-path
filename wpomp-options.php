46149b0a2b9b28d6ca7b800360105df219020a58
rvola <rvola@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-28 07:37:50 +0000

    options cleaning settings api
    git-svn-id: http://plugins.svn.wordpress.org/wp-original-media-path/trunk@1232858 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpomp-options.php
@@ -0,0 +1,15 @@
+<?php if(!defined('ABSPATH')) exit;?>
+<div class="wrap">
+	<h2><?php echo $this->_PLUGIN_NAME;?></h2>
+	<p>
+		<a href="http://wordpress.org/plugins/wp-original-media-path/faq/" target="_blank" class="button"><?php _e('FAQ', 'wpomp');?></a>
+		<a href="http://wordpress.org/plugins/wp-original-media-path/installation/" target="_blank" class="button"><?php _e('How to Configure', 'wpomp');?></a>
+	</p>
+	<form method="post" action="options.php">
+	<?php
+		settings_fields('wpomp_fields');
+		do_settings_sections('wpomp_pages');
+		submit_button();
+	?>
+	</form>
+</div>
