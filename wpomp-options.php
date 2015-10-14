bc42ad89fff55e3772b9d22d8e20c0cad5da9cf4
rvola <rvola@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-14 15:36:47 +0000

    GitHub link
    git-svn-id: http://plugins.svn.wordpress.org/wp-original-media-path/trunk@1265948 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpomp-options.php
+++ wpomp-options.php
@@ -2,6 +2,7 @@
 <div class="wrap">
 	<h2><?php echo $this->_PLUGIN_NAME;?></h2>
 	<p>
+		<a href="https://github.com/rvola/wp-original-media-path" target="_blank" class="button">GitHub</a>
 		<a href="http://wordpress.org/plugins/wp-original-media-path/faq/" target="_blank" class="button"><?php _e('FAQ', 'wpomp');?></a>
 		<a href="http://wordpress.org/plugins/wp-original-media-path/installation/" target="_blank" class="button"><?php _e('How to Configure', 'wpomp');?></a>
 	</p>
