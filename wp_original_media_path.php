4ea328fa57229b0f8271c8a9afbe3b427058dd8a
Nicolas <nicolas@rvola.com>
2022-01-27 14:48:03 +0100

    ✓ Check compatbility WP 5.9

--- wp_original_media_path.php
+++ wp_original_media_path.php
@@ -16,7 +16,7 @@ Text Domain:            wp-original-media-path
 Domain Path:            /languages/
 
 Requires at least:      3.5
-Tested up to:           5.7
+Tested up to:           5.9
 Requires PHP:           7.0
 
 License:		        GPLv3
