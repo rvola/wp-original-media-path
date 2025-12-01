950360270dadc1a340f5e5d522e114d853acc8f1
Nicolas GEHIN <nicolas@rvola.com>
2025-12-01 15:56:32 +0100

    Check WP 6.9

--- wp_original_media_path.php
+++ wp_original_media_path.php
@@ -16,7 +16,7 @@ Text Domain:            wp-original-media-path
 Domain Path:            /languages/
 
 Requires at least:      3.5
-Tested up to:           6.8
+Tested up to:           6.9
 Requires PHP:           7.0
 
 License:		        GPLv3
