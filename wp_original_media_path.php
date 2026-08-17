8c39a43a458e32108e8e033473d6fc8f0d22ec77
Nicolas GEHIN <nicolas@rvola.com>
2026-08-17 10:08:10 +0200

    chore: minimal PHP 7.4

--- wp_original_media_path.php
+++ wp_original_media_path.php
@@ -17,7 +17,7 @@ Domain Path:            /languages/
 
 Requires at least:      3.5
 Tested up to:           6.9
-Requires PHP:           7.0
+Requires PHP:           7.4
 
 License:		        GPLv3
 License URI:	        https://www.gnu.org/licenses/gpl-3.0
